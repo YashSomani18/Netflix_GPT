38d8767b6f17a9eb8b9b2e9410ec407c8c2c7f84
YashSomani18 <yashsomani18@gmail.com>
2023-10-20 21:47:12 +0530

    SecondaryContainer && custom hooks

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import MovieList from "./MovieList";
+import { useSelector } from "react-redux";
 
 const SecondaryContainer = () => {
-  return (
-    <div>SecondaryContainer</div>
-  )
-}
+  const movies = useSelector((store) => store.movies);
+  if (movies === null) return;
 
-export default SecondaryContainer
+  return (
+    <div>
+      <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+      <MovieList title={"Popular"} movies={movies.popularMovies} />
+      <MovieList title={"Top Rated"} movies={movies.topRatedMovies} />
+      <MovieList title={"Upcoming Movies"} movies={movies.upcomingMovies} />
+    </div>
+  );
+};
+
+export default SecondaryContainer;
